11a5d8c821ae1ed72417f9be88ccfac0ff024391
daltonfelipe <daltonphellipe@gmail.com>
2019-06-25 18:07:43 -0300

    ajusta simulação e README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-## Gravitus
+# gravitus2D
+
+O *gravitus* é um protótipo de simulação de interações gravitacionais feito com a biblioteca [Pygame](https://www.pygame.org/wiki/about).
+
+# Dependências (bibliotecas)
+* random
+* math
+* pygame _v1.9.6_
+
+# Funcionamento
+
+O simulador possui 4 principais módulos:
+* Constants
+* Particle
+* Simulator
+* Vector
+
+O primeiro módulo é responsável por armazenar as constantes do programa, como por exemplo, constante gravitacional (**G**), cores e configurações da tela de simulação.
+
+O Módulo **Particle** representa o objeto no espaço (**Simulator**) e faz as interações com outras particulas ao seu redor.
+
+O **Simulator** é o espaço de simulação e é reponsável por fazer com que as parículas possam interagir entre si.
+
+Finalmente, o módulo **Vector** é um módulo auxiliar para trabalhar com vetore bidimensionais.
--- run.py
+++ run.py
@@ -4,28 +4,6 @@ from app.libs.simulator import Simulator
 from app.libs.vector import Vector
 from app import run 
 
-import random
-
-
-def random_vector(xmax, xmin, ymax, ymin):
-    return Vector(
-        random.uniform(xmax, xmin),
-        random.uniform(ymax, ymin)
-    )
-
-
-# particles = []
-# num_particles = 2
-# for i in range(num_particles):
-#     particles.append(
-#         Particle(
-#             name='p-{}'.format(i),
-#             radius=20,
-#             position=random_vector(200, 600, 200, 400),
-#             vellocity=Vector(0, 0),
-#             color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-#         )
-#     )
 
 p1 = Particle(
         name='Sol',
@@ -52,13 +30,6 @@ p3 = Particle(
         color=Constants.RED_COLOR
     )
 
-# p3 = Particle(
-#         name='Marte',
-#         radius=10,
-#         position=Vector(125, 675),
-#         vellocity=Vector(0, 300),
-#         color=Constants.RED_COLOR
-#     )
 
 particles = [p1, p2, p3]
 
